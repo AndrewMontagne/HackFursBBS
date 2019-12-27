5e1d132430fed68fe7ab7a72175eed0ffc20787a
Andrew O'Rourke <andrew@montagne.uk>
2019-12-27 20:32:50 +0000

    Added ANSI commands for alternate buffer.

--- splash.py
+++ splash.py
@@ -3,6 +3,7 @@
 import urwid
 import urwid.raw_display
 import urwid.web_display
+from urwid import escape
 import os
 
 class simple_display(urwid.raw_display.Screen):
@@ -30,13 +31,18 @@ txt = urwid.Text(('banner', u'''
 ╚██╔═██╔╝██║  ██║██║  ██║╚██████╗██║  ██╗██║     ╚██████╔╝██║  ██║███████║
  ╚═╝ ╚═╝ ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝
  
-                       Press any key to continue...                       '''), align='center')
+                       Press any key to continue...                       
+                       '''), align='center')
 map1 = urwid.AttrMap(txt, 'streak')
 fill = urwid.Filler(map1)
 map2 = urwid.AttrMap(fill, 'bg')
 
 screen = simple_display()
 
-urwid.MainLoop(map2, palette, screen,
-    unhandled_input=exit_app).run()
+print(chr(27) + "[?1049h")
+urwid.MainLoop(map2, palette, screen, unhandled_input=exit_app).run()
+print(chr(27) + "[2J")
+print(chr(27) + "[?1049l")
+
+
 
