6facb9a524672a4abd0972a043fa946bd640a051
Andrew O'Rourke <andrew@montagne.uk>
2019-12-27 22:12:55 +0000

    Upgrade!

--- splash.py
+++ splash.py
@@ -16,31 +16,52 @@ class simple_display(urwid.raw_display.Screen):
 
 
 def exit_app(key):
+    if (key == 'esc'):
+        raise urwid.ExitMainLoop()
+
+
+def handleRegistrationButton(button):
+    registerButton.original_widget.set_label(u'Registration Closed')
+
+
+def handleExitButton(button):
     raise urwid.ExitMainLoop()
 
 palette = [
     ('banner', 'white', 'black'),
-    ('streak', 'black', 'black'),
+    ('buttonf', 'black', 'white'),
+    ('button', 'black', 'dark gray'),
     ('bg', 'black', 'black'),]
 
-txt = urwid.Text(('banner', u'''
- ██╗ ██╗ ██╗  ██╗ █████╗  ██████╗██╗  ██╗███████╗██╗   ██╗██████╗ ███████╗
+blank = urwid.Divider()
+
+logo = urwid.Text(('banner', u''' ██╗ ██╗ ██╗  ██╗ █████╗  ██████╗██╗  ██╗███████╗██╗   ██╗██████╗ ███████╗
 ████████╗██║  ██║██╔══██╗██╔════╝██║ ██╔╝██╔════╝██║   ██║██╔══██╗██╔════╝
 ╚██╔═██╔╝███████║███████║██║     █████╔╝ █████╗  ██║   ██║██████╔╝███████╗
 ████████╗██╔══██║██╔══██║██║     ██╔═██╗ ██╔══╝  ██║   ██║██╔══██╗╚════██║
 ╚██╔═██╔╝██║  ██║██║  ██║╚██████╗██║  ██╗██║     ╚██████╔╝██║  ██║███████║
- ╚═╝ ╚═╝ ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝
- 
-                       Press any key to continue...                       
-                       '''), align='center')
-map1 = urwid.AttrMap(txt, 'streak')
-fill = urwid.Filler(map1)
-map2 = urwid.AttrMap(fill, 'bg')
+ ╚═╝ ╚═╝ ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝'''), align='center')
+
+subtext = urwid.Text(('banner', u'-=-=- Established 2019 -=-=-'), align='center')
+
+registerButton = urwid.Button('Register', handleRegistrationButton)
+registerButton = urwid.AttrMap(registerButton, 'button', 'buttonf')
+
+exitButton = urwid.Button('Exit', handleExitButton)
+exitButton = urwid.AttrMap(exitButton, 'button', 'buttonf')
+
+buttonPile = urwid.Pile([registerButton, blank, exitButton])
+buttonPile = urwid.Padding(buttonPile, align='center', width=24)
+
+pile = urwid.Pile([logo, blank, subtext, blank, buttonPile])
+
+center = urwid.Filler(pile, valign='middle', height='pack')
+center = urwid.AttrMap(center, 'bg')
 
 screen = simple_display()
 
 print(chr(27) + "[?1049h")
-urwid.MainLoop(map2, palette, screen, unhandled_input=exit_app).run()
+urwid.MainLoop(center, palette, screen, unhandled_input=exit_app).run()
 print(chr(27) + "[2J")
 print(chr(27) + "[?1049l")
 
