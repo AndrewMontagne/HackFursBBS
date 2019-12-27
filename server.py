f844a77bb3997b4ef142746e5e2e8b05ffc6c7ba
Andrew O'Rourke <andrew@montagne.uk>
2019-12-27 01:16:48 +0000

    Initial commit.

---
+++ server.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import asyncio, asyncssh, os, sys, subprocess
+
+
+class MySFTPServer(asyncssh.SFTPServer):
+    def __init__(self, chan):
+        root = '/tmp/sftp/' + chan.get_extra_info('username')
+        os.makedirs(root, exist_ok=True)
+        super().__init__(chan, chroot=root)
+
+
+class MySSHServer(asyncssh.SSHServer):
+    def connection_made(self, conn):
+        print('SSH connection received from %s.' %
+              conn.get_extra_info('peername')[0])
+
+    def connection_lost(self, exc):
+        if exc:
+            print('SSH connection error: ' + str(exc), file=sys.stderr)
+        else:
+            print('SSH connection closed.')
+
+    def begin_auth(self, username):
+        return False  # user does not require auth to connect
+
+
+async def handle_client(process):
+    process.stdin.channel.set_line_mode(False)
+
+    env=process.env
+    env['TERM'] = process.get_terminal_type()
+    env['COLUMNS'] = str(process.get_terminal_size()[0])
+    env['LINES'] = str(process.get_terminal_size()[1])
+    env['SHELL'] = '/bin/bash'
+    env['USER'] = 'andrew'
+
+    bc_proc = subprocess.Popen(['python3', 'splash.py'], shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, env=env, start_new_session=True, encoding=None)
+
+    await process.redirect(stdin=bc_proc.stdin, stdout=bc_proc.stdout,
+                           stderr=bc_proc.stderr)
+    await process.stdout.drain()
+    process.exit(0)
+
+
+async def start_server(port):
+    print("Starting server on port " + str(port) + "...")
+    await asyncssh.listen('', port, server_host_keys=['ssh_host_key'],
+                          sftp_factory=MySFTPServer,
+                          server_factory=MySSHServer,
+                          process_factory=handle_client,
+                          allow_scp=False)
+
+loop = asyncio.get_event_loop()
+
+port = 2022
+
+if len(sys.argv) > 1:
+    port = int(sys.argv[1])
+
+try:
+    loop.run_until_complete(start_server(port))
+except (OSError, asyncssh.Error) as exc:
+    sys.exit('Error starting server: ' + str(exc))
+
+loop.run_forever()
---
+++ splash.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import urwid
+import urwid.raw_display
+import urwid.web_display
+import os
+
+class simple_display(urwid.raw_display.Screen):
+
+    def get_cols_rows(self):
+        if "COLUMNS" in os.environ:
+            return int(os.getenv('COLUMNS')), int(os.getenv('LINES'))
+        else:
+            return super().get_cols_rows()
+
+
+def exit_app(key):
+    raise urwid.ExitMainLoop()
+
+palette = [
+    ('banner', 'white', 'black'),
+    ('streak', 'black', 'black'),
+    ('bg', 'black', 'black'),]
+
+txt = urwid.Text(('banner', u'''
+ ██╗ ██╗ ██╗  ██╗ █████╗  ██████╗██╗  ██╗███████╗██╗   ██╗██████╗ ███████╗
+████████╗██║  ██║██╔══██╗██╔════╝██║ ██╔╝██╔════╝██║   ██║██╔══██╗██╔════╝
+╚██╔═██╔╝███████║███████║██║     █████╔╝ █████╗  ██║   ██║██████╔╝███████╗
+████████╗██╔══██║██╔══██║██║     ██╔═██╗ ██╔══╝  ██║   ██║██╔══██╗╚════██║
+╚██╔═██╔╝██║  ██║██║  ██║╚██████╗██║  ██╗██║     ╚██████╔╝██║  ██║███████║
+ ╚═╝ ╚═╝ ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝
+ 
+                       Press any key to continue...                       '''), align='center')
+map1 = urwid.AttrMap(txt, 'streak')
+fill = urwid.Filler(map1)
+map2 = urwid.AttrMap(fill, 'bg')
+
+screen = simple_display()
+
+urwid.MainLoop(map2, palette, screen,
+    unhandled_input=exit_app).run()
+
